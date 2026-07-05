e2e70333f19cccf6bd477a4b886600e475254583
PranavRasal <pranavcode19@gmail.com>
2026-07-05 22:08:33 +0530

    feat: initialize frontend with React, Vite, and Tailwind CSS
    - Added package.json for project dependencies and scripts
    - Created favicon.svg and icons.svg for application branding
    - Set up App.css to import Tailwind CSS styles
    - Developed main App component with context provider
    - Created MyContext for state management
    - Implemented basic structure for Chat and ChatWindow components
    - Added SideBar component with a sample heading
    - Configured Vite for React and Tailwind CSS integration

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import SideBar from './assets/sideBar'
+import ChatWindow from './assets/chatWindow'
+import { MyContext } from './MyContext'
+
+function App() {
+  const providerValue ={} ;
+
+  return (
+    <div >
+      <MyContext.Provider value={providerValue}>
+        <SideBar />
+      </MyContext.Provider>
+    </div>
+  )
+}
+
+export default App
---
+++ Frontend/src/assets/sideBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function sideBar() {
+  return (
+    <div>
+      <h1 className='text-3xl bg-amber-50'>pranav euhgsuyi</h1>
+    </div>
+  )
+}
+
+export default sideBar
